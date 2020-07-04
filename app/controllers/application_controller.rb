6c587d972a80109183251563e7e99b7b853901c4
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-04 20:33:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,26 +8,48 @@ class ApplicationController < Sinatra::Base
 		set :session_secret, "password_security"
 	end
 
+#get "/" renders an index.erb file with links to signup or login.
+#index.erb
 	get "/" do
 		erb :index
 	end
 
+#renders a form to create a new user (w username and pass)
+#signup.erb
 	get "/signup" do
 		erb :signup
 	end
 
+#make a new instance of user class w/ username and pass from params.
 	post "/signup" do
-		#your code here!
-	end
+ user = User.new(:username => params[:username], :password => params[:password])
+#redirect to login once the user is saved
+#redirect to failure if the user cannot be saved.
+ if user.save
+    redirect "/login"
+  else
+    redirect "/failure"
+  end
+end
 
+#renders a form for logging in login.erb
 	get "/login" do
 		erb :login
 	end
 
+#find the user by username
+#did we find the user by that username? success or failure?
 	post "/login" do
-		#your code here!
-	end
+		user = User.find_by(:username => params[:username])
+		if user
+	 redirect "/success"
+ else
+	 redirect "/failure"
+ end
+end
 
+#displayed once a user logs in
+#success.erb
 	get "/success" do
 		if logged_in?
 			erb :success
@@ -36,20 +58,25 @@ class ApplicationController < Sinatra::Base
 		end
 	end
 
+#failure.erb
+#accessed if there is an error logging in or signing up
 	get "/failure" do
 		erb :failure
 	end
 
+#clears the session data and redirects to the homepage
 	get "/logout" do
 		session.clear
 		redirect "/"
 	end
 
 	helpers do
+		#returns true or false based on the presence of a session[:user_id]
 		def logged_in?
 			!!session[:user_id]
 		end
 
+# returns the instance of the logged in user, based on the session[:user_id].
 		def current_user
 			User.find(session[:user_id])
 		end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-	
+	has_secure_password
 end
+
+#ActiveRecord macro gives us access to new methods
+#Macro: method that when called, creates new methods for you.
+#Metaprogramming
+#works with bcrypt
