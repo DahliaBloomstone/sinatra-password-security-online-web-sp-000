af630b9ccb6533aea28feb992aa250df486d9572
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-04 20:36:02 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ end
 #works with bcrypt
 
 #validate password match by using a method called authenticate.
-#it is metaprogramming so we cannot see it! 
+#it is metaprogramming so we cannot see it!
+#Authenticate works by:
+    #Takes a String as an argument e.g. i_luv@byron_poodle_darling
+    #It turns the String into a salted, hashed version (76776516e058d2bf187213df6917a7e)
+    #It compares this salted, hashed version with the user's stored salted, hashed password in the database
+    #If the two versions match, authenticate will return the User instance; if not, it returns false
